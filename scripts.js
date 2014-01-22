caa4ddc9f5a0313a84921a2de4ea136b8eb388d8
Karens <yetti4@gmail.com>
2014-01-22 12:32:38 +0100

    Added heatmap visualization

--- scripts.js
+++ scripts.js
@@ -4,25 +4,19 @@ function initialize() {
     center: new google.maps.LatLng(51.37180, 13.23583)
   };
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	heatmapData = [];
+	heatmap = new google.maps.visualization.HeatmapLayer({
+		data: heatmapData
+	});
+	heatmap.setMap(map);
 
-	var marker;
-	var myLatlng;
-	/*$.getJSON( "locations.json", function( data ) {
-		$.each( data, function( key, ip ) {
-			myLatlng = new google.maps.LatLng(ip.lat,ip.lon);
-			 marker = new google.maps.Marker({
-				position: myLatlng,
-				map: map,
-				title: ip.org
-			});
-		});
- 
-	});*/
 }
 
 
-//google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 ip_addresses = []; // list with ipaddress that need to be verified
 checkForNewIPs();
 
@@ -34,14 +28,12 @@ socket.onopen= function() {
 socket.onmessage= function(s) {
 	transaction = jQuery.parseJSON(s.data);
 	ip_address = transaction.x.relayed_by;
-	console.log("New ip found: " + ip_address);
 	ip_addresses.push(ip_address);   
 };
 
 
 function checkForNewIPs(){
 	
-	console.log("Check for new ips: "+ ip_addresses.length);
 	if(ip_addresses.length != 0){
 		ip_address = ip_addresses.shift();
 		getLocationIP(ip_address);
@@ -55,19 +47,18 @@ function getLocationIP(ipAddress){
 	if(ipAddress != "127.0.0.1"){		
 		console.log("Get location from: " + ipAddress);
 		$.getJSON("http://ip-api.com/json/"+ipAddress, function( data ) {
-			//console.log(data);
-			addToHeatMap(data.lat,data.lon);
-	 
+			addToHeatMap(data.lat,data.lon);	 
 		});
 	}
 }
 
-function addToHeatMap(lat,long){
+function addToHeatMap(lat,lng){
 	
-	console.log("Add lat: " + lat + " and long: " + long);
+	
+	heatmapData.push(new google.maps.LatLng(lat, lng));
+	heatmap.setData(heatmapData);
 	
 }
-
 
 
 
