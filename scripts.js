6fa316b10cd4c0edde95005bfc73a926b61b4ca7
Karens <yetti4@gmail.com>
2014-01-22 12:49:55 +0100

    Changed heatmap performance

--- scripts.js
+++ scripts.js
@@ -7,9 +7,30 @@ function initialize() {
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	heatmapData = [];
 	heatmap = new google.maps.visualization.HeatmapLayer({
-		data: heatmapData
+		data: heatmapData,
+		
 	});
+	var gradient = [
+		'rgba(0, 255, 255, 0)',
+		'rgba(0, 255, 255, 1)',
+		'rgba(0, 191, 255, 1)',
+		'rgba(0, 127, 255, 1)',
+		'rgba(0, 63, 255, 1)',
+		'rgba(0, 0, 255, 1)',
+		'rgba(0, 0, 223, 1)',
+		'rgba(0, 0, 191, 1)',
+		'rgba(0, 0, 159, 1)',
+		'rgba(0, 0, 127, 1)',
+		'rgba(63, 0, 91, 1)',
+		'rgba(127, 0, 63, 1)',
+		'rgba(191, 0, 31, 1)',
+		'rgba(255, 0, 0, 1)'
+	];
+	heatmap.set('gradient', gradient);
+	heatmap.set('radius', 20);
+	heatmap.set('dissipating', true);
 	heatmap.setMap(map);
+	
 
 }
 
