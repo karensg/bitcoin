cbf8c524cb1e873bea9af19617276614198fd306
Ruud Visser <ruud@scrambledapps.nl>
2014-02-02 16:36:42 +0100

    Added: Only useful info out the blocks in json file

--- java/bitcoin/src/bitcoin/Blocks.java
+++ java/bitcoin/src/bitcoin/Blocks.java
@@ -4,6 +4,7 @@ package bitcoin;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,17 +32,42 @@ public class Blocks {
            json = WebApi.readJsonFromUrl("http://blockchain.info/inv/" + block.getString("hash") + "?format=json");
            invData.put(i.toString(), json);
            System.out.println(blocks.getJSONArray("blocks").length() - i + ": " +json.toString());
-           Thread.sleep(500);
+           //Thread.sleep(500);
        }
        
        //Pare useful information
+       JSONObject inventory;
+       JSONArray usefulData = new JSONArray("[blocks]");
+       for (Integer i=0; i < invData.length(); i++) {
+           inventory = invData.getJSONObject(i.toString());
+           
+           JSONObject blockinfo = new JSONObject();
+           blockinfo.put("hash",inventory.get("hash"));
+           blockinfo.put("relayed_percent",inventory.get("relayed_percent"));
+           blockinfo.put("initial_time",inventory.get("initial_time"));
+           blockinfo.put("relayed_count",inventory.get("relayed_count"));
+           blockinfo.put("initial_ip",inventory.get("initial_ip"));
+           
+           JSONArray owners = (JSONArray)inventory.getJSONArray("probable_owners");
+           for(Integer k = 0; k < owners.length(); k++) {
+               JSONObject owner = owners.getJSONObject(k);
+               if(!owner.getString("ip").equals("0.0.0.0")){
+                   blockinfo.put("owner",owner.getString("ip"));
+                   blockinfo.put("confidence",owner.getInt("confidence"));
+                   break;
+               }
+           }
+           blockinfo.put("relayed_count",inventory.get("relayed_count"));
+           
+           usefulData.put(i,blockinfo);
+       }
        
        
        //write to json file
        try {
  
 		FileWriter file = new FileWriter("../../blocks.json", false);
-		file.write(invData.toString());
+		file.write(usefulData.toString());
 		file.flush();
 		file.close();
  
